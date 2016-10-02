10cac0c3349285297a7b7ff1d717a735e1aa1e8f
BaoQiang <mailbaoqiang@gmail.com>
2016-10-02 19:39:31 +0800

    debugging ......

--- xiaowxapi/defaultbot.py
+++ xiaowxapi/defaultbot.py
@@ -25,10 +25,10 @@ class DefaultBot(WxApi):
             self.send_img_msg_by_uid('img/1.png', msg['user']['id'])
             self.send_file_msg_by_uid('file/1.txt', msg['user']['id'])
 
-    # def schedule(self):
-    #     if not self.send_msg(u'小号', u'test msg'):
-    #         print('[ERROR] schedule task exec failed!!!')
-    #     time.sleep(1)
+    def schedule(self):
+        if not self.send_msg(u'小号', u'test msg'):
+            print('[ERROR] schedule task exec failed!!!')
+        time.sleep(1)
 
 
 def main():
@@ -44,6 +44,7 @@ def test():
     # bot = DefaultBot()
     print(requests.get(url).text)
 
+
 def test2():
     # url = 'https://login.wx.qq.com/jslogin?appid=wx782c26e4c19acffb&fun=new&lang=zh_CN&_=1475388677149'
     url = 'https://wx.qq.com/cgi-bin/mmwebwx-bin/webwxsync?sid=1rNuNCANQHqPdZqP&skey=@crypt_9db55326_60a2947061ce89b1ba81afc27fbde155&pass_ticket=kacKwq%2B8CoOX6T8KQUGYpGPmYO06gEQBdNXvkn%2Fg54H5NLeM7wFySN3BtNcVPPiJ'
@@ -52,9 +53,21 @@ def test2():
     # print(requests.post(url,params).text)
 
     # url = 'https://wx.qq.com/cgi-bin/mmwebwx-bin/webwxsync?sid=2xYX22XMjDQi0PC7&skey=@crypt_9db55326_e39450380edc077767efad6fdcb2c391&pass_ticket=DVesc5THA0F0Yb24aKrsElU3g%252FpPm60Hk1HebPqsgzaRm8hdqoMx3M2mA15RoDTq'
-    print(requests.post(url,params).text)
+    print(requests.post(url, params).text)
+
+
+def test3():
+    a = 'ni你'
+    b = u'hi, i received: %s' % a
+    if isinstance(b, str):
+        b = str.encode(b, 'utf-8')
+    elif isinstance(b, bytes):
+        b = bytes
+
+    c = b.encode()
 
 if __name__ == '__main__':
     main()
     # test()
     # test2()
+    # test3()
